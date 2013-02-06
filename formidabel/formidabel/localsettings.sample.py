b3bd439f95c8b21d06bbdd2cfee27a3c600fc3a6
Nicolas Noé <nicolas@niconoe.org>
2013-02-06 15:21:24 +0100

    Initialized empty Django-CMS project.

---
+++ formidabel/formidabel/localsettings.sample.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'formidabel',                     
+        'USER': 'postgres',                  
+        'PASSWORD': '',         
+        'HOST': 'localhost',          
+        'PORT': '', 
+    }
+}
