32e64e0206c42191dfddc15635e104a14d60be89
Nicolas Noé <nicolas@niconoe.org>
2013-02-21 16:51:12 +0100

    Basic search form now triggers the correct REST action.

--- formidabel/ants_atlas/static/ants_atlas/js/ants_atlas.js
+++ formidabel/ants_atlas/static/ants_atlas/js/ants_atlas.js
@@ -10,12 +10,19 @@
         var OccurrenceList = Backbone.Collection.extend({
 			model: Occurrence,
 
+            // Store the species_id, so it will be available to build retrieval URL
             initialize: function(species_id){
                 this.species_id = species_id;
             },
 
             url: function(){
                 return '/api/v1/species/' + this.species_id;
+            },
+
+            // The server "to be stored" data is returned in the occurrences attribute
+            // That's what we should use to build each Occurrence
+            parse: function(response){
+                return response.occurrences;
             }
         });
         
@@ -23,15 +30,47 @@
             // Fill the data from server
             // Should we overrive fetch instead ?
             loadOccurrences: function(){
-                var col = new OccurrenceList(this.get('species_id'));
-
-                this.set({occurrences: col.fetch()});
+                col = new OccurrenceList(this.get('species_id'));
+                this.set('collection', col); // We'll now have a collection attribute
+                
+                col.fetch({success: function(){
+                    // The collection is now filled with Occurrences
+                    console.log(col);
+                }});
+                
+                //this.set({occurrences: col.fetch()});
             }
         });
 
-        var bootstrap = function(){
-			var s = new OccurrenceSearch({species_id: 372, color: '#ab13cc'});
-            s.loadOccurrences();
+        /*var OccurrenceSearchView = Backbone.View.extend({
+            el: $('#search_list'), // TODO: ! Decouple !!!
+
+            render: function(){
+                $(this.el).append('<p>Hello world !</p>');
+            }
+        });*/
+
+        var SearchView = Backbone.View.extend({
+            events: {
+                'submit #search': 'trigger_search'
+            },
+
+            trigger_search: function(){
+                var s = new OccurrenceSearch({
+                    species_id: this.$('#species_id').val(),
+                    color: '#ab13cc' // Make color configurable
+                });
+
+                s.loadOccurrences();
+
+                return false; // Don't submit the search form
+            }
+        });
+
+        var bootstrap = function(app_container){
+            // Main view that observes the search form
+            var f = new SearchView({el: app_container});
+
         };
 
         // Export public members
